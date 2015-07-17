72560a4baa801c8229ce28a37233c4c65dd6795d
coderaiser <mnemonic.enemy@gmail.com>
2015-07-17 08:54:33 -0400

    feature(ufo) add

---
+++ bin/ufo.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+    
+    var ufo         = require('..'),
+        commands    = ufo([
+            'put --type directory --path /tmp/hello/world/why/not',
+            'put --type file --path /tmp/hello/tmp.js --format base64 --data aGVsbG8=',
+            'patch --hash bb27f5fa7f50d9747525754f9d14c63187cc2959 --path /tmp/hello/tmp.js --data QEAgLTMwMywxNiArMzAzLDE3IEBACiAtZGF0YSAnIAorIAogJTBBICAgICAgIAo='
+        ].join('\n'));
+    
+    commands.on('error', function(error) {
+        console.error(error.message);
+        commands.abort();
+    });
+})();
